c2d86e1aff263f35849d9bef225f44185038ee7c
nechemiaseif <nechemiaseif@gmail.com>
2019-03-28 10:00:49 -0400

    Reorder method calls in Nechemia class

--- edu/touro/mco232/Nechemia.java
+++ edu/touro/mco232/Nechemia.java
@@ -3,18 +3,17 @@ package edu.touro.mco232;
 public class Nechemia extends Person implements GitCamper{
 
     public Nechemia() {
-        expressAttitudeTowardSout();
         printGithubUsername();
+        expressAttitudeTowardSout();        
+    }
+    
+    @Override
+    public void printGithubUsername() {
+        System.out.println("nechemiaseif");
     }
     
     @Override
     public void expressAttitudeTowardSout() {
         System.out.println("Sout is pretty dope");
     }
-
-    @Override
-    public void printGithubUsername() {
-        System.out.println("nechemiaseif");
-    }
-    
 }
