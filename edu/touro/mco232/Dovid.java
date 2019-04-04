694661cbe5f8687ed5723b30c91ccaba6f28967f
Dovid Hashimi <dhash5705@gmail.com>
2019-04-04 12:12:49 -0400

    Add Dovid Class

---
+++ edu/touro/mco232/Dovid.java
@@ -0,0 +1,18 @@
+package edu.touro.mco232;
+    public class Dovid extends Person implements GitCamper{
+
+        public Dovid() {
+            printGithubUsername();
+            expressAttitudeTowardSout();
+        }
+
+        @Override
+        public void printGithubUsername() {
+            System.out.println("homelessdave");
+        }
+
+        @Override
+        public void expressAttitudeTowardSout() {
+            System.out.println("Sout is frickin pretty dope");
+        }
+}
